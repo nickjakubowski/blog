a1e1b0f82d6eb8fde676a889c15b06435fecaf8a
Nicholas Jakubowski <ncjakubowski@gmail.com>
2017-01-02 15:57:09 -0800

    Add gorilla/mux, break up handlerFunc

--- src/server/main.go
+++ src/server/main.go
@@ -3,23 +3,25 @@ package main
 import (
     "fmt"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h2>Welcome to the future site of another blog</h2>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:side@sidenick.com\">" +
-		    "side@sidenick.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you were looking for. Here's a link to the homepage " +
-			"<a href=\"http://localhost:3000\">sidenick.com</a>")
-	}
+	fmt.Fprint(w, "<h2>Welcome to the future site of another blog</h2>")
+}
+
+func contact(w http.ResponseWriter, r * *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+    "to <a href=\"mailto:somewhere@nowhere.com\">"+
+    "thisgoesnowhere@lost.confused</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
