6055b4f6e10e8b5dc9937bded083e7511f302968
Nicholas Jakubowski <ncjakubowski@gmail.com>
2017-01-02 16:41:14 -0800

    Modify 404 page

--- src/server/main.go
+++ src/server/main.go
@@ -19,9 +19,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
     "thisgoesnowhere@lost.confused</a>.")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>404</h1><h5>Sorry, there is nothing here....except for this text, but nothing besides that</h5>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
